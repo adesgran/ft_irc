91f6e74985c1d9dbc8c425a42e4a3907eca69bce
adesgran <adesgran.dev@gmail.com>
2023-07-13 13:52:16 +0200

    Fix

--- tmp/severtest/server.cpp
+++ tmp/severtest/server.cpp
@@ -6,9 +6,10 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 16:56:03 by adesgran          #+#    #+#             */
-/*   Updated: 2023/07/06 19:55:07 by adesgran         ###   ########.fr       */
+/*   Updated: 2023/07/13 13:51:53 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <iostream>
 #include <string>
@@ -20,19 +21,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-
-#define PORT 8080 
-
+#define PORT 6667 
 
 int main(int argc, char const* argv[])
 {
+	(void)argc;
+	(void)argv;
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[5000] = { 0 };
-    char* hello = strdup("Hello from server");
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -73,15 +74,41 @@ int main(int argc, char const* argv[])
         exit(EXIT_FAILURE);
     }
 	std::cout << "New connection set !" << std::endl;
-	int r = 0;
+	//int flags = fcntl(0, F_GETFL, 0);
+//fcntl(0, F_SETFL, flags | O_NONBLOCK);
 	while (1)
 	{
+		std::string msg;
+		std::cout << "SERVER >> ";
+		if (std::getline(std::cin, msg))
+		{
+			if (msg.size() > 1)
+			{
+				msg.push_back('\n');
+    			send(new_socket, msg.c_str(), msg.size(), 0);
+			}
+		}
 		buffer[0] = '\0';
-    	valread = recv(new_socket, buffer, 5000, 0);
-        //valread = recv(new_socket, buffer, 5000,MSG_DONTWAIT);
+        valread = recv(new_socket, buffer, 5000,MSG_DONTWAIT);
 		buffer[valread] = '\0';
 		if (buffer[0])
-			std::cout << "CLIENT << " << buffer << std::endl;
+		{
+			std::string res(buffer);
+			while (res.size())
+			{
+				size_t pos = res.find('\n');
+				std::string tmp = res.substr(0, pos);
+				std::cout << "CLIENT << " << tmp << std::endl;
+				res.erase(0, pos + 1);
+				if (tmp.find("PING") != std::string::npos){
+					std::string s(tmp.begin() + 4, tmp.end());
+					s = "PONG" + s;
+					std::cout << "SERVER >> " << s << std::endl;
+					s.push_back('\n');
+					send(new_socket, s.c_str(), s.size(), 0);
+				}
+			}
+		}
 
 	} 
 
@@ -91,3 +118,4 @@ int main(int argc, char const* argv[])
     shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
+
