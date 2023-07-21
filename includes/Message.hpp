30417cc6031015fe1a894529f281f050a47acba4
Mathilde CHASSIGNET <mchassig@paul-f4Ar5s4.clusters.42paris.fr>
2023-07-21 14:10:55 +0200

    inits Message class

--- includes/Message.hpp
+++ includes/Message.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Message.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 12:20:36 by adesgran          #+#    #+#             */
-/*   Updated: 2023/07/13 12:21:17 by adesgran         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:08:17 by mchassig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 
 # include <iostream>
 # include <string>
+# include <User.hpp>
+
+enum cmdValue {
+	ERR_UNKNOWNCMD,
+	CAP,
+	NICK,
+	USER,
+	JOIN,
+	PRIVMSG,
+	KICK,
+	INVITE,
+	TOPIC,
+	MODE,
+	PING,
+};
+
+class User;
 
 class Message {
 	public:
@@ -26,6 +43,32 @@ class Message {
 		void	read( std::string message );
 
 	private:
+		cmdValue						_cmdName;
+		std::map<std::string, cmdValue>	_cmdMap;
+
+		// sender
+		// receiver
+		// payload
+		// command called
+		// [...]
+
+		// Parsing ----------------------------------------------
+		void						_cmdparser(std::string cmdline, User &user);
+		std::vector<std::string>	_split(const std::string& str, char sep);
+		
+		// IRC commands -----------------------------------------
+		void	_cmdInit();
+		void	_cap(std::vector<std::string> arg, User &user);
+		void	_nick(std::vector<std::string> arg, User &user);
+		void	_user(std::vector<std::string> arg, User &user);
+		// PRIVMSG
+		// KICK
+		// INVITE
+		// TOPIC
+		// MODE
+		// PING
+		
+
 
 };
 
--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Message.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 12:21:22 by adesgran          #+#    #+#             */
-/*   Updated: 2023/07/13 12:25:56 by adesgran         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:09:10 by mchassig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Message::Message(void)
 
 Message::Message(const Message &message)
 {
-	(void)message;
+	*this = message;
 }
 
 Message::~Message(void)
@@ -27,11 +27,85 @@ Message::~Message(void)
 
 Message &Message::operator=(const Message &message)
 {
-	(void)message;
+	if ( this == &message )
+		return ( *this );
 	return (*this);
 }
 
 void	Message::read( std::string message )
 {
 	std::cout << message << std::endl;
+}
+
+// Parsing ----------------------------------------------
+
+void	Message::_cmdparser(std::string cmdline, User &user)
+{
+    std::vector<std::string> cmd_lines = _split(cmdline, '\n');
+	for (std::vector<std::string>::iterator	line = cmd_lines.begin();
+			line != cmd_lines.end();
+			line++)
+	{
+		std::vector<std::string>	cmd_arg = _split(*line, ' ');
+		switch (_cmdMap[cmd_arg[0]])
+		{
+			case CAP:
+				_cmdName = _cmdMap[cmd_arg[0]];
+				_cap(cmd_arg, user);
+				break;
+			case NICK:
+				_cmdName = _cmdMap[cmd_arg[0]];
+				_nick(cmd_arg, user);
+				break;
+			case USER:
+				_cmdName = _cmdMap[cmd_arg[0]];
+				_user(cmd_arg, user);
+				break;
+			default:
+				_cmdName = ERR_UNKNOWNCMD;
+				break;
+		}
+	}
+}
+
+std::vector<std::string>	Message::_split(const std::string& str, char sep)
+{
+    std::vector<std::string> tokens;
+ 
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, sep)) {
+        tokens.push_back(token);
+    }
+ 
+    return tokens;
+}
+
+// IRC commands -----------------------------------------
+
+void	Message::_cmdInit()
+{
+	_cmdMap["CAP"] = CAP;
+	_cmdMap["NICK"] = NICK;
+	_cmdMap["USER"] = USER;
+}
+
+void	Message::_cap(std::vector<std::string> arg, User &user)
+{
+	std::cout << "	*CAP cmd detected*\n";
+	(void)arg;
+	(void)user;
+}
+
+void	Message::_nick(std::vector<std::string> arg, User &user)
+{
+	std::cout << "	*NICK cmd detected*\n";
+	user.setNickname(arg[1]);
+}
+
+void	Message::_user(std::vector<std::string> arg, User &user)
+{
+	std::cout << "	*USER cmd detected*\n";
+	(void)arg;
+	(void)user;
 }
